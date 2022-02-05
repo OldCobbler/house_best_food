788554925b0f7b792b40659a99718431c7174ff0
OldCobbler <oldcobbler@yandex.ru>
2022-02-05 13:06:30 +0300

    added smooth scroll

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,20 @@
 $(function(){
 
+
+  $(".menu").on("click", "a", function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+    event.preventDefault();
+
+    //забираем идентификатор бока с атрибута href
+    var id = $(this).attr('href'),
+
+      //узнаем высоту от начала страницы до блока на который ссылается якорь
+      top = $(id).offset().top;
+
+    //анимируем переход на расстояние - top за 1500 мс
+    $('body,html').animate({ scrollTop: top }, 1500);
+  });
+
   $('.header__menu-btn').on('click', function(){
     $('.menu__list').toggleClass('menu__list--active');
   });
